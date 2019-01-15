100cad9db3f8a07162c4573ce0820caf47ae70c9
Rodrigo <rodrigo.m2n@gmail.com>
2019-01-15 21:14:56 -0200

    check permissions before run on android

--- android/app/src/main/java/com/rncontactstest/MainActivity.java
+++ android/app/src/main/java/com/rncontactstest/MainActivity.java
@@ -1,8 +1,24 @@
 package com.rncontactstest;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+
+    public static final int PERMISSION_REQ_CODE = 1234;
+    public static final int OVERLAY_PERMISSION_REQ_CODE = 1235;
+
+    String[] perms = {
+            "android.permission.READ_CONTACTS",
+            "android.permission.WRITE_CONTACTS"
+    };
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -11,5 +27,41 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "RNContactsTest";
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Checking permissions on init
+        checkPerms();
+    }
+
+    public void checkPerms() {
+        // Checking if device version > 22 and we need to use new permission model 
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            // Checking if we can draw window overlay
+            if (!Settings.canDrawOverlays(this)) {
+                // Requesting permission for window overlay(needed for all react-native apps)
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+            }
+            for (String perm : perms) {
+                // Checking each persmission and if denied then requesting permissions
+                if (checkSelfPermission(perm) == PackageManager.PERMISSION_DENIED) {
+                    requestPermissions(perms, PERMISSION_REQ_CODE);
+                    break;
+                }
+            }
+        }
+    }
+
+    // Window overlay permission intent result
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            checkPerms();
+        }
     }
 }
